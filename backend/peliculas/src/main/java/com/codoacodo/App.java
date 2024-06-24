3a197180a31e8c62870b842e8bbb722ca9ed0e92
isaias ruben <ruben_4912@hotmail.com>
2024-06-24 19:06:21 -0300

    se sube back

---
+++ backend/peliculas/src/main/java/com/codoacodo/App.java
@@ -0,0 +1,25 @@
+package com.codoacodo;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        try{
+            Statement st= conectorDB.getSt();
+           ResultSet rs= st.executeQuery("Select * from peliculas");
+           while(rs.next()){
+            System.out.println(rs.getString("titulo"));
+           }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ backend/peliculas/src/main/java/com/codoacodo/conectorDB.java
@@ -0,0 +1,31 @@
+package com.codoacodo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class conectorDB {
+    private static Connection conn ;
+    private static Statement st;
+    
+    public static void main(String[] args) {
+
+        // Conexión a la base de datos
+        try {
+            conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/peli24119","root","");
+
+             st=conn.createStatement();
+        
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static Connection getConn(){
+        return conn;
+    }
+
+    public static Statement getSt(){
+        return st;
+    }
+
+};
